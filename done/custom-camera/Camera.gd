818748c32a78879ebdee0cae694c95433a2ee02a
willieLjohnson <liwa.johnson@gmail.com>
2019-05-08 13:41:39 -0400

    Simplify custome camera follow script

--- done/custom-camera/Camera.gd
+++ done/custom-camera/Camera.gd
@@ -1,21 +1,13 @@
 extends Node2D
 
-onready var screen_size = Vector2(ProjectSettings.get("display/window/size/width"), ProjectSettings.get("display/window/size/height"))
+onready var screen_size = OS.get_window_size()
 onready var player = $Player
-onready var last_player_position = player.get_position()
 
 func _ready():
-	var canvas_transform = get_viewport().get_canvas_transform()
-	canvas_transform[2] = - last_player_position + screen_size / 2
-	get_viewport().set_canvas_transform(canvas_transform)
+	update_camera()
 
-func update_camera():
-	var player_offset = last_player_position - player.get_position()
-	last_player_position = player.get_position()
-
-	var canvas_transform = get_viewport().get_canvas_transform()
-	canvas_transform[2] += player_offset
-	get_viewport().set_canvas_transform(canvas_transform)
-
-	return player_offset
 	
+func update_camera():
+	var canvas_transform = get_viewport().get_canvas_transform()
+	canvas_transform[2] = - player.get_position() + screen_size / 2
+	get_viewport().set_canvas_transform(canvas_transform)
