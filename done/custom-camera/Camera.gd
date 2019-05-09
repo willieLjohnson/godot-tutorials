478186504e0f1d2fd58009db1eac513059e41fbb
willieLjohnson <liwa.johnson@gmail.com>
2019-05-09 09:50:23 -0400

    Add zelda like camera movement

--- done/custom-camera/Camera.gd
+++ done/custom-camera/Camera.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 onready var window_size = OS.get_window_size()
 onready var player = $Player
-onready var player_world_position = get_player_world_position()
+onready var player_world_position = get_player_grid_position()
 
 func _ready():
 	var canvas_transform = get_viewport().get_canvas_transform()
@@ -11,7 +11,19 @@ func _ready():
 
 func get_player_grid_position():
 	# Converts theplayer position in px to current position on world grid.
-	pass
+	var position = player.get_position()
+	var x = floor(position.x / window_size.x)
+	var y = floor(position.y / window_size.y)
+	return Vector2(x, y)
 
 func update_camera():
-	pass
+	var new_player_grid_position = get_player_grid_position()
+	var transform = Transform2D()
+
+	if new_player_grid_position != player_world_position:
+		player_world_position = new_player_grid_position
+		transform = get_viewport().get_canvas_transform()
+		transform[2] = -player_world_position * window_size
+		get_viewport().set_canvas_transform(transform)
+
+	return transform
