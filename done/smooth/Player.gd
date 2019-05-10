71de9c8ed7ca11be87a3eaeaeb024d7be222a2d3
willieLjohnson <liwa.johnson@gmail.com>
2019-05-09 22:25:58 -0400

    Add seek and flee states

--- done/smooth/Player.gd
+++ done/smooth/Player.gd
@@ -1,9 +1,11 @@
 extends KinematicBody2D
 
-const MAX_SPEED = 20000
+const MAX_SPEED = 30000
 const MAX_FORCE = 0.02
 var velocity = Vector2()
 onready var target = get_position()
+
+export (int, "SEEK", "FLEE") var mode = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,6 +18,11 @@ func _physics_process(delta):
 
 func steer(target):
 	var desired_velocity = (target - get_position()).normalized() * MAX_SPEED
+	if mode == 0:
+		pass
+	elif mode == 1:
+		desired_velocity = -desired_velocity
+
 	var steer = desired_velocity - velocity
 	var target_velocity = velocity + (steer * MAX_FORCE)
 	return target_velocity
